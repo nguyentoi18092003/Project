8f1a3eaa99b45eca839dcdbb4ab46303df1f69b0
Nguyen Thi Toi <nguyentoi18092003hvcnbcvt@gmail.com>
2024-02-04 11:32:34 +0700

    Cau hinh floder pug gom: Header, Footer, Title

--- product-management/controllers/client/home.controller.js
+++ product-management/controllers/client/home.controller.js
@@ -2,7 +2,10 @@ module.exports.index=(req,res)=>{
     //been file index.route mk da co phan /home roi, thi ben day tiep tuc noi sau prodct
     // link sang trang product co link la /home/
     // tu bien router mk dat tren mk goi ham get
-    res.render("client/pages/home/index")
+    res.render("client/pages/home/index",{
+        pageTitle:"Trang chủ"
+        //controller là lấy data trả về cho view, nên ta có thể thêm objetc để cấu hình cho tiêu đề ở giao diện linh hoạt hơn
+    });
     // giai thich ham render ben file goc giong ben tren
 }
 // ham module.exports la de xuat, index la ten ham mk dat
--- product-management/controllers/client/product.controller.js
+++ product-management/controllers/client/product.controller.js
@@ -2,7 +2,10 @@ module.exports.index=(req,res)=>{
     //been file index.route mk da co phan /product roi, thi ben day tiep tuc noi sau prodct
     // link sang trang product co link la /product/
     // tu bien router mk dat tren mk goi ham get
-    res.render("client/pages/products/index")
+    res.render("client/pages/products/index",{
+        pageTitle:"Danh sach san pham"
+        //controller là lấy data trả về cho view, nên ta có thể thêm objetc để cấu hình cho tiêu đề ở giao diện linh hoạt hơn
+    })
     // giai thich ham render ben file goc giong ben tren
    }
 //index la ten mk dat thoi, con file module export la mot ham co san
